db44d6df8a9b43c93c96697a71bea71ce2558a85
bcfgamer1 <bcf.fco@gmail.com>
2025-03-17 22:48:59 -0300

    Meu primeiro programa em Python.

---
+++ Aula5.py
@@ -0,0 +1,39 @@
+# Listas
+carros = ["Accord", "Polo", "Civic", "Fiat Uno"]
+print(carros)
+# contagem de quantidade "len"
+print(len(carros))
+# contagem de indices e aparecer listados porem tudo começa no 0 "[0]"
+print(carros[1])
+print(carros[2])
+print(carros[3])
+carros[0] = "Jetta"
+print(carros[0])
+
+# notas = [6,7,5,8,9]
+# soma = 0
+# x = 0
+# while x < len(notas):
+#     soma += notas [x]
+#     x += 1
+# print(f"Média = {(soma/x):.2f}")
+#
+# notas = [0,0,0,0,0]
+# soma = 0
+# x = 0
+# while x < len(notas):
+#     notas[x] = float(input(f"Nota {x+1} ="))
+#     soma += notas[x]
+#     x += 1
+# print(f"Média = {(soma/x):.2f}")
+
+carros_back = carros
+print(carros_back)
+carros[3] = "Jeep"
+print(carros)
+
+carros_back = carros[0:5]
+print(carros_back)
+carros[1] = "Golf"
+print(carros)
+print(carros_back)
--- primeiro_programa.py
+++ primeiro_programa.py
@@ -1,1 +1,34 @@
-print("Hello worrld!")
+print("Hello world!")
+print()
+
+# Exercicio_1
+print("Bruno")
+print()
+
+# Exercicio_2
+a = 3
+b = 5
+print(2*a*3*b)
+print()
+
+# Exercicio_3
+# num1 = 2
+# num2 = 3
+# num3 = 4
+# soma = num1+num2+num3
+# print(soma)
+# Seleciona a linha e dê Crtl + ? para deixar "comentado" e pra tirar a mesma coisa.
+
+#'''int( para inteiros'''
+# num1 = int(input("Número 1 = "))
+# num2 = int(input("Número 2 = "))
+# num3 = int(input("Número 3 = "))
+# soma = num1+num2+num3
+# print(soma)
+
+# '''float( para decimais'''
+num1 = float(input("Número 1 = "))
+num2 = float(input("Número 2 = "))
+num3 = float(input("Número 3 = "))
+soma = num1+num2+num3
+print(f"Soma = {soma:.2f}")
